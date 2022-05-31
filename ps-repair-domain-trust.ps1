e5dda830dccc09d5c9405b86d4c641a82fbad94d
tpjkosakowski <83101663+tpjkosakowski@users.noreply.github.com>
2022-05-31 18:48:29 -0400

    version 2

--- ps-repair-domain-trust.ps1
+++ ps-repair-domain-trust.ps1
@@ -1,9 +1,77 @@
-# Powershell script to repair broken domain trust on remote computer through Kaseya
+$version = "2.0"
+$githubver = "https://raw.githubusercontent.com/curi0usJack/luckystrike/master/currentversion.txt"
+$updatefile = "https://raw.githubusercontent.com/curi0usJack/luckystrike/master/update.ps1"
+function UpdatesAvailable()
+{
+	$updateavailable = $false
+	$nextversion = $null
+	try
+	{
+		$nextversion = (New-Object System.Net.WebClient).DownloadString($githubver).Trim([Environment]::NewLine)
+	}
+	catch [System.Exception] 
+	{
+		Write-Message $_ "debug"
+	}
+	
+	Write-Message "CURRENT VERSION: $version" "debug"
+	Write-Message "NEXT VERSION: $nextversion" "debug"
+	if ($nextversion -ne $null -and $version -ne $nextversion)
+	{
+		#An update is most likely available, but make sure
+		$updateavailable = $false
+		$curr = $version.Split('.')
+		$next = $nextversion.Split('.')
+		for($i=0; $i -le ($curr.Count -1); $i++)
+		{
+			if ([int]$next[$i] -gt [int]$curr[$i])
+			{
+				$updateavailable = $true
+				break
+			}
+		}
+	}
+	return $updateavailable
+}
 
+function Process-Updates()
+{
+	if (Test-Connection 8.8.8.8 -Count 1 -Quiet)
+	{
+		$updatepath = "$($PWD.Path)\update.ps1"
+		if (Test-Path -Path $updatepath)	
+		{
+			#Remove-Item $updatepath
+		}
+		if (UpdatesAvailable)
+		{
+			Write-Message "Update available. Do you want to update luckystrike? Your payloads/templates will be preserved." "success"
+			$response = Read-Host "`nPlease select Y or N"
+			while (($response -match "[YyNn]") -eq $false)
+			{
+				$response = Read-Host "This is a binary situation. Y or N please."
+			}
+
+			if ($response -match "[Yy]")
+			{	
+				(New-Object System.Net.Webclient).DownloadFile($updatefile, $updatepath)
+				Start-Process PowerShell -Arg $updatepath
+				exit
+			}
+		}
+	}
+	else
+	{
+		Write-Message "Unable to check for updates. Internet connection not available." "warning"
+	}
+}
+
+# Powershell script to repair broken domain trust on remote computer through Kaseya
+<#
 $username = "1054433"   #enter employee id number here
 $password = ConvertTo-SecureString "password" -AsPlainText -Force  #replace password with your current password
 $creds = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
 
 Reset-ComputerMachinePassword -Server adc-002.townepark.net -credential $creds
 
-Test-ComputerSecureChannel -Repair -Credential $creds
+Test-ComputerSecureChannel -Repair -Credential $creds#>
