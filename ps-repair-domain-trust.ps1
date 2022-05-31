7e984fcda84329cc707562fa4146a61dd7c0736c
tpjkosakowski <83101663+tpjkosakowski@users.noreply.github.com>
2022-05-31 18:17:58 -0400

    initial

---
+++ ps-repair-domain-trust.ps1
@@ -0,0 +1,7 @@
+$username = "1054433"   #enter employee id number here
+$password = ConvertTo-SecureString "password" -AsPlainText -Force  #replace password with your current password
+$creds = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
+
+Reset-ComputerMachinePassword -Server adc-002.townepark.net -credential $creds
+
+Test-ComputerSecureChannel -Repair -Credential $creds
