dd123a688627fab08784416c6ef2bc48a580faa7
tpjkosakowski <83101663+tpjkosakowski@users.noreply.github.com>
2022-05-31 18:20:53 -0400

    added comment

--- ps-repair-domain-trust.ps1
+++ ps-repair-domain-trust.ps1
@@ -1,3 +1,5 @@
+# Powershell script to repair broken domain trust on remote computer through Kaseya
+
 $username = "1054433"   #enter employee id number here
 $password = ConvertTo-SecureString "password" -AsPlainText -Force  #replace password with your current password
 $creds = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
